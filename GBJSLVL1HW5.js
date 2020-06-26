83c60d7834d681e9e2cf5bab22682a3824e709e3
JaggKerby <ilyakerby@gmail.com>
2020-06-26 12:43:05 +0300

    для 5 урока

---
+++ GBJSLVL1HW5.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let button = document.getElementsByClassName('product__btn')
+
+button.addEventListener('click', function (event) {
+    console.log('click!');
+    consile.dir(event);
+});
